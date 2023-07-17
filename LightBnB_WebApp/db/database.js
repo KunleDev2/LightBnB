178e3ef0b9bca34fc3f3bd79f4462135e1e862cc
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-07-16 20:09:22 -0500

    implement query list of reservations

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -57,7 +57,6 @@ const getUserWithId = function(id) {
 
   return pool.query(queryString, values)
     .then((result) => {
-      console.log(result.rows);
       return Promise.resolve(result.rows[0]);
     })
     .catch((err) => {
@@ -98,7 +97,25 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = $1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT $2;
+  `;
+  const values = [guest_id, limit];
+
+  return pool.query(queryString, values)
+    .then((result) => {
+      return Promise.resolve(result.rows);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Properties
