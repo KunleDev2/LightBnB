9fa6bc075b372f76ba7c62332a40e3f83f97e220
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-07-17 08:40:32 -0500

    add get all reservations query

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -130,11 +130,6 @@ const getAllProperties = (options, limit = 10) => {
 
   const initQueryString = [];
 
-  // const queryString = `
-  // SELECT * FROM properties 
-  // LIMIT $1;
-  // `;
-
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
@@ -148,18 +143,18 @@ const getAllProperties = (options, limit = 10) => {
 
   if (options.guest_id) {
     initQueryString.push(`${options.guest_id}`);
-    queryString += `WHERE guest_id = $${initQueryString.length} `;
+    queryString += `AND guest_id = $${initQueryString.length} `;
   };
 
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
     initQueryString.push(`${options.minimum_price_per_night * 100}`);
     initQueryString.push(`${options.maximum_price_per_night * 100}`);
-    queryString += `WHERE cost_per_night BETWEEN $${initQueryString.length - 1} AND $${initQueryString.length}`;
+    queryString += `AND cost_per_night BETWEEN $${initQueryString.length - 1} AND $${initQueryString.length}`;
   };
 
   if (options.minimum_rating) {
     initQueryString.push(`${options.minimum_rating}`);
-    queryString += `WHERE property_reviews.rating >= $${initQueryString.length} `;
+    queryString += `AND property_reviews.rating >= $${initQueryString.length} `;
   };
 
   initQueryString.push(limit);
@@ -168,10 +163,6 @@ const getAllProperties = (options, limit = 10) => {
   ORDER BY cost_per_night
   LIMIT $${initQueryString.length};
   `;
-
-  // console.log(queryString, initQueryString);
-
-  // const values = [limit];
 
   return pool.query(queryString, initQueryString)
     .then((result) => {
@@ -188,16 +179,16 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  
+
   const queryString = `
   INSERT INTO properties (owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,street,city,province,post_code,country,parking_spaces,number_of_bathrooms,number_of_bedrooms) 
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
   RETURNING *;
   `;
-  const values = [property.owner_id,property.title,property.description,property.thumbnail_photo_url,property.cover_photo_url,property.cost_per_night,property.street,property.city,property.province,property.post_code,property.country,property.parking_spaces,property.number_of_bathrooms,property.number_of_bedrooms];
+  const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms];
 
   return pool.query(queryString, values)
-    .then((result) => { 
+    .then((result) => {
       return Promise.resolve(result.rows[0]);
     })
     .catch((err) => {
